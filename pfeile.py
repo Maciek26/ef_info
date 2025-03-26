e71183dc60018b96bae1dcc7e58c496c3692e99c
Maciek26 <85782513+Maciek26@users.noreply.github.com>
2025-03-26 22:12:54 +0100

    Add files via upload
    created the file and added the matrixes

---
+++ pfeile.py
@@ -0,0 +1,19 @@
+farbe = [["b","w","g","w"]]
+richtung = []
+
+
+
+def print_in_arrows():
+    s = ""
+    i = 0
+    for j in farbe[0]:
+        #s =  s + "b"
+        s += str(j)
+        i+= 1
+        
+        
+    print(s)
+
+print(str(farbe[0][0]))
+print("\u21E9")
+print_in_arrows()
