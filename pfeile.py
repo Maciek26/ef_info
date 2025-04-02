b3679e0a4b132a13979c307ad5d596f5e812da78
Mace <mawikierszka@gmail.com>
2025-04-02 23:02:55 +0200

    added an example aswell as some minor fixes

--- pfeile.py
+++ pfeile.py
@@ -1,9 +1,20 @@
 farbe = [["w","w","w","w","w","w"],["w","w","w","w","w","w"],["w","w","w","w","w","w"],["w","w","w","w","w","w"],["w","w","w","w","w","w"],["w","w","w","w","w","w"]]
-richtung = [["1","4","6","1","2","3"],["1","4","6","1","2","3"],["1","4","6","1","2","3"],["1","4","6","1","2","3"],["1","4","6","1","2","3"],["1","4","6","1","2","3"]]
+richtung = [["4","6","4","4","6","6"],["5","4","3","4","8","8"],["5","1","4","5","7","6"],["1","6","5","6","6","8"],["4","5","2","7","7","5"],["2","2","1","3","3","1"]]
+#Richtungen von der Aufgabe Nr. 1 6x6 Matrix
+
 matrixs = [farbe, richtung]
 
 matrix_grösse = len(farbe) - 1          #Bei einer 6x6 Matrix = 5 weil computer bei 0 anfängt zu zählen
 
+#Richtungen:
+# 1 - Up
+# 2 - Up-right
+# 3 - Right
+# 4 - Down-right
+# 5 - Down
+# 6 - Down-left
+# 7 - Left
+# 8 - Up-left
 
 def pos_up(q,x,y,n,rtn_str=False):
     a = 0                  #x = x-Achse; y = y-Achse; q = ob die Farbe oder Richtung angesprochen wird
@@ -87,7 +98,7 @@ def pos_down_left(q,x,y,n,rtn_str=False):
     a = 0               
     while a < n:
         a += 1
-        if y >= 0 or x <= 0:                       
+        if y >= matrix_grösse or x <= 0:                       
             return False   
         else:            
             x -= 1
@@ -113,9 +124,13 @@ def pos_left(q,x,y,n,rtn_str=False):
                 return matrixs[q][y][x]
     
 def pos_up_left(q,x,y,n,rtn_str=False):
+    print("received")
     a = 0               
     while a < n:
         a += 1
+        print(x)
+        print(y)
+        print(y<=0)
         if y <= 0 or x <= 0:                       
             return False   
         else:            
@@ -123,6 +138,7 @@ def pos_up_left(q,x,y,n,rtn_str=False):
             y -= 1
         if a == n:
             if rtn_str == True:                 #gibt an ob es die Kordinaten schickt oder inhalt
+            
                 return x,y
             else:
                 return matrixs[q][y][x]
@@ -153,13 +169,15 @@ def print_in_str(q):
 
 def Regel1():
     currentTry = True
-    for y in range(1, matrix_grösse):
-        for x in range(1, matrix_grösse):
+    for y in range(0, matrix_grösse+1):
+        for x in range(0, matrix_grösse+1):
             richtung = int(matrixs[1][y][x])
-            
-            if movingUp_Down[richtung](0,x,y,2) == False:
-                toBeColored = movingUp_Down[richtung](0,x,y,1,True)
-                print(toBeColored[1])
+            print(x,y)
+            print(richtung)
+            if movingUp_Down[richtung - 1](0,x,y,2) == False:
+                toBeColored = movingUp_Down[richtung - 1](0,x,y,1,True)
+                print(toBeColored[0])
+
                 color("b", toBeColored[0],toBeColored[1])
                 print(f"Der sieht nh schwarzen von {x} / {y}")
             
