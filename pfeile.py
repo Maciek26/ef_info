b80cd55386968e11827bab25b16ed99b6ac00fe4
Mace <maciek.kierszka@ict-scouts.ch>
2025-04-16 17:41:40 +0200

    added comments

--- pfeile.py
+++ pfeile.py
@@ -17,17 +17,17 @@ matrix_grösse = len(farbe) - 1          #Bei einer 6x6 Matrix = 5 weil computer
 # 7 - Left
 # 8 - Up-left
 
-def pos_up(q,x,y,n,rtn_str=False):
-    a = 0                                       #x = x-Achse; y = y-Achse; q = ob die Farbe oder Richtung angesprochen wird
-    while a < n:
-        a += 1
+def pos_up(q,x,y,n,rtn_str=False):              #Funktion, Hier: geht eine Position nach oben in der Matrix --> Pos_up ---kurz für---> Position_up
+    a = 0                                       #x = x-Achse; y = y-Achse; q = ob die Farbe oder Richtung angesprochen wird; n = wie oft wir in eine Richtung gehen möchten (z.B. n=2 wäre hier zwei mal nach oben gehen in der Matrix); rtn_str = ob wir als Resultat den Inhalt des erreichten Feldes (default) oder die Koordinanten des erreichten Feldes zurück geben
+    while a < n:                                #Könnte in der Theorie immer True sein, da wir später mit hilfe einer "If-Schleife" den Loop unterbrechen. Für die schönheit und redundanz überprüfen wir hier jedoch ebenfalls ob wir bereits die gewünschte Anzahl an Versuchen gemacht haben
+        a += 1                                  #Wir notieren, dass wir einmal den durchgang wiederhollt haben, somit können wir auch nur so oft wie gewollt in die gewollte Richtung gehen
         if y > 0:                               #überprüfen ob wir nicht bereits ganz oben sind
-            y -= 1
+            y -= 1                              #Um eine Position in der Matrix hoch zu gehen, müssen wir in unserer Liste minus eins rechnen, bei uns ist die Koordinate vom Ecken oben links (0/0) und nicht unten links wie in einem "normalen" Koordinatensystem
         else:
             return False                        #Falls wir am Rand sind, geben wir False zurück
-        if a == n:
+        if a == n:                              #Falls wir bereits die n-mal in die Richtung gegangen sind und nicht auf den Rand gestossen sind, geben wir nun entweder die erreichte Koordinate zurück oder dessen Inhalt
             if rtn_str == True:                 #Wir gucken ob die Koordinaten benötigt werden
-                return x,y
+                return x,y                      #Falls "rtn_str" gleich "True" war gibt es nun die x,y Koordinaten von der neuen Position. In diesem Fall eine Position über der welche eingereicht wurde
             else:
                 return matrixs[q][y][x]         #Falls nicht dann wird die Farbe oder Richtung zurück gegeben
                                                 #Je nachdem ob der User für q 0 oder 1 angegeben hat; 0 -> Farbe ; 1 -> Richtung
@@ -65,7 +65,7 @@ def pos_down_right(q,x,y,n,rtn_str=False):
     a = 0               
     while a < n:
         a += 1
-        if y >= matrix_grösse or x >= matrix_grösse:                       
+        if y >= matrix_grösse or x >= matrix_grösse:        # Wir überprüfen ob wir am unterem Rand oder am rechtem Rand sind           
             return False   
         else:            
             x += 1
@@ -80,7 +80,7 @@ def pos_down(q,x,y,n,rtn_str=False):
     a = 0              
     while a < n:
         a += 1
-        if y < matrix_grösse:                
+        if y < matrix_grösse:                               # Wir überprüfen ob wir am unterem Rand sind
             y += 1
         else:
             return False
@@ -94,7 +94,7 @@ def pos_down_left(q,x,y,n,rtn_str=False):
     a = 0               
     while a < n:
         a += 1
-        if y >= matrix_grösse or x <= 0:                       
+        if y >= matrix_grösse or x <= 0:                   # Wir überprüfen ob wir am unterem oder linkem Rand sind
             return False   
         else:            
             x -= 1
@@ -107,9 +107,9 @@ def pos_down_left(q,x,y,n,rtn_str=False):
     
 def pos_left(q,x,y,n,rtn_str=False):
     a = 0              
-    while a < n:
+    while a < n:                                                        
         a += 1
-        if x > 0:                
+        if x > 0:                                          # Wir überprüfen ob wir am linkem Rand sind
             x -= 1
         else:
             return False
@@ -123,13 +123,13 @@ def pos_up_left(q,x,y,n,rtn_str=False):
     a = 0               
     while a < n:
         a += 1
-        if y <= 0 or x <= 0:                       
+        if y <= 0 or x <= 0:                             # Wir überprüfen ob wir am oberem oder linkem Rand sind
             return False   
         else:            
             x -= 1
             y -= 1
         if a == n:
-            if rtn_str == True:                 #gibt an ob es die Kordinaten schickt oder inhalt
+            if rtn_str == True:                 
             
                 return x,y
             else:
@@ -141,45 +141,40 @@ movingUp_Down = [pos_up, pos_up_right, pos_right, pos_down_right, pos_down, pos_
 
 def color(c,x,y):               # Mit dieser Funktion färben wir die Felder ein, c sagt uns in welcher Farbe wir das Feld einfärben sollen
     if c == "b":
-        matrixs[0][y][x] = "b"
+        matrixs[0][y][x] = "b"  # Hier benötigen wir die Variable "q" nicht um uns zu sagen welche Matrix angesprochen wird (Farb- oder Richtungsmatrix) da wir ja natürlich die Farbe verändern
     if c == "w":
         matrixs[0][y][x] = "w"
 
-def print_in_str(q):            # Wir für das Debugen benutzt um die Matrizen Farbe oder Richtung auszugeben, q gibt an welche von den beiden
-    s = ""
-    i = 0
-    for n in range(0,6):
-        for j in matrixs[q][n]:
-            s += str(j) + " "
-            i+= 1
-        s += "\n"
+def print_in_str(q):                            # Wird für das Debugen benutzt um die Matrizen Farbe oder Richtung auszugeben, q gibt an welche von den beiden Matrizen angesprochen wird
+    s = ""                                      # Ein leerer String welcher mit allen Pfeilen gefüllt wird und dann "geprinted"                   
+    for n in range(0,matrix_grösse+1):          # Geht durch alle Zeilen in der Matrix durch, matrix_grösse plus eins passt es immer auf die Richtige grösse an
+        for j in matrixs[q][n]:                 # Geht durch alle Ellemente in einer Zeile der Matrix
+            s += str(j) + " "                   # Jedes Element wird an den String hinzugefügt
+        s += "\n"                               # Am Ende jeder Zeile fügen wir "\n" hinzu für einen Zeilenumbruch
     return s
 
-def Regel3():
-    grey_count = 0
-    w = 0
-    test = 0
-    while w < 2:
-        w += 1
-        grey_seen = 0
-        black_seen = 0
-        for y in range(0, matrix_grösse + 1):
-            for x in range(0, matrix_grösse + 1):
-                if matrixs[0][y][x] == "g":
-                    grey_count += 1
-                n = 0
+def Regel3():                   # Unsere 3te Regel bassiert auf dem ausschliessen, hier überprüfen wir wie viele Unbekannte ("g") wir haben. Falls dann ein Pfeil nur einen Unbekannten und keinen schwarzen sieht, muss dieser Unbekannte ein schwarzer Pfeil sein. Das Gleiche gilt aber auch anders herum, falls ein Pfeil einen schwarzen Pfeil bereits sieht, sind alle anderen gesehenen Pfeile weiss ("w") --> ist das gleiche wie Regel 2, wird hier jedoch wiederhollt um das Skript zu beschleunigen.
+    grey_count = 0              # Zählvariabel für wie viele graue (Unbekannte) Pfeile wir gesehen haben               
+    w = 0                       # Zählvariabel damit das gesamte Skript zwei mal wiederholt wird.                
+    while w < 2:                
+        w += 1                  # Zählvariabel vergrössern
+        for y in range(0, matrix_grösse + 1):           # Geht alle Zeilen der Matrix durch 
+            for x in range(0, matrix_grösse + 1):       # Geht alle Spalten der Matrix durch 
+                if matrixs[0][y][x] == "g":             # Durch die Kombination der vorherigen beiden "for" loops, wird hier jedes Element der Matrix kontrolliert auf dessen Inhalt. Falls es "g" (Grau/Unbekannt) ist,
+                    grey_count += 1                     # Die Zählvariable "grey_count" wird erhöht
+                n = 0                                   # Zählvariable um mitzuzählen wie oft wir die folgende While Schleife wiederholt haben
                 black_seen = 0
                 grey_seen = []
-                richtung = int(matrixs[1][y][x])
-                while n < matrix_grösse + 1:                                        #In dieser Schleife gehen wir in eine Richtung und zählen wie viele Schwarze Pfeile wir sehen
-                    n += 1                                                          #Zählvariable um in die Richtung n-mal zu gehen
-                    if movingUp_Down[richtung - 1](0,x,y,n) == "b":
-                        black_seen += 1 
-                    elif movingUp_Down[richtung - 1](0,x,y,n) == "g":
-                        grey_seen.append(movingUp_Down[richtung - 1](0,x,y,n, True))
-                    elif  movingUp_Down[richtung - 1](1,x,y,n) == False:             #Falls False dann ist er am Rand angekommen
-                        if len(grey_seen) == 1 and black_seen == 0:                     #Falls ein Pfeil nur einen Unbekannten sieht und keinen Schwarzen
-                            color("b", grey_seen[0][0], grey_seen[0][1])                #dann wird dieser Unbekannte Pfeil schwarz
+                richtung = int(matrixs[1][y][x]) - 1    # Wir speichern die Richtung ab in welcher der zurzeit gewählte Pfeil zeigt (man muss minus eins rechnen da die Liste bei 0 beginnt zu zählen, könnte verhindert werden in dem ein decoy Element am Anfang eingefügt wird)
+                while n < matrix_grösse + 1:                                        #In dieser Schleife gehen wir in eine Richtung und zählen wie viele Schwarze und Grau Pfeile wir sehen, wird maximal so oft wiederholt wie die Matrix gross ist, auch hier hätte es "while True" sein können, da es durch "if statements" unterbrochen wird. Aus redudanz und zur vermeitung endless-loops nicht "while True" 
+                    n += 1                                                          #Zählvariable n erhöhen
+                    if movingUp_Down[richtung](0,x,y,n) == "b":                     #In der Liste "movingUp_Down" wählen wir die Richtung in welche wir uns bewegen wollen, in diese Richtung Bewegen wir uns dann entsprechend oft zu welcher durchgang es der Schleife ist (wird in "n" gespeichert)
+                        black_seen += 1                                             #Zählvariable wird um eins erhöht
+                    elif movingUp_Down[richtung](0,x,y,n) == "g":                   
+                        grey_seen.append(movingUp_Down[richtung](0,x,y,n, True))
+                    elif  movingUp_Down[richtung](1,x,y,n) == False:             #Falls "False" dann ist er am Rand angekommen
+                        if len(grey_seen) == 1 and black_seen == 0:                  #Falls ein Pfeil nur einen Unbekannten sieht und keinen Schwarzen, dann wird dieser Unbekannte Pfeil schwarz
+                            color("b", grey_seen[0][0], grey_seen[0][1])                
                         elif len(grey_seen) == 1 and black_seen == 1:
                             color("w", grey_seen[0][0],grey_seen[0][1])
     if grey_count == 0:
@@ -195,7 +190,7 @@ def Regel2():
             n = 0
             black_seen = 0
             richtung = int(matrixs[1][y][x])
-            while n < matrix_grösse + 1:                                       #In dieser Schleife gehen wir in eine Richtung und zählen wie viele Schwarze Pfeile wir sehen                
+            while n < matrix_grösse + 1:                                         #In dieser Schleife gehen wir in eine Richtung und zählen wie viele Schwarze Pfeile wir sehen                
                 n += 1                                                           #Zählvariable um in die Richtung n-mal zu gehen    
                 if movingUp_Down[richtung - 1](0,x,y,n) == "b":
                     black_seen += 1 
